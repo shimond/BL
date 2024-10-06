6984cef52962a5cdb62852f700bcc0fd99b7e7d8
Shimon Dahan <shimond@any-techs.co.il>
2024-10-06 16:33:29 +0300

    Add IGarageService

---
+++ Day4/API/Interfaces/IGarageService.cs
@@ -0,0 +1,13 @@
+﻿using API.Model.Dtos;
+
+namespace API.Interfaces;
+
+public interface IGarageService
+{
+    Task<List<CarDto>> GetCarsAsync(int pageNumber, int pageSize);
+    Task<CarDto?> GetCarByIdAsync(int id);
+    Task<CarDto> AddCarAsync(CreateCarDto carDto);
+    Task<CarDto> UpdateCarAsync(int id, UpdateCarDto carDto);
+    Task<CarDto> UpdateCarYearAsync(int id, int year);
+    Task  DeleteCarAsync(int id);
+}
