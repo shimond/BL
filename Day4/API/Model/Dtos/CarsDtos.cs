21f85f3231d898aedc0f5612b7c1b60f6e98c94c
Shimon Dahan <shimond@any-techs.co.il>
2024-10-06 16:32:57 +0300

    Add Cars model and Dtos

---
+++ Day4/API/Model/Dtos/CarsDtos.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model.Dtos;
+
+public record CreateCarDto
+{
+    [Required]
+    public string Make { get; set; }
+    [Required]
+    public string Model { get; set; }
+    [Range(1886, 2100)]
+    public int Year { get; set; }
+}
+
+public record UpdateCarDto
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
+
+public record UpdateCarYearDto
+{
+    public int NewYear { get; set; }
+}
+
+
+public record CarDto
+{
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
---
+++ Day4/API/Model/Entities/CarEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Model.Entities;
+
+public record Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
+
